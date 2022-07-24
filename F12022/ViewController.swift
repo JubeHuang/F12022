4072b98b9fa8bc7bbabcfc17d7bc31d5bb98d1da
Jube <jube820106@gmail.com>
2022-07-24 15:39:50 +0800

    Initial Commit

---
+++ F12022/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  F12022
+//
+//  Created by Tungyi Huang on 2022/7/20.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.backButtonTitle = "back"
+    
+    }
+
+
+}
